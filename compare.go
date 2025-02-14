dd0ded8887a807437b23d769c78e46f6817e1a2b
David Fritz <noemail>
2025-02-14 14:06:34 -0700

    refactor compare

--- compare.go
+++ compare.go
@@ -1,5 +1,7 @@
 package real
 
+import "bytes"
+
 func (x *Real) Compare(y *Real) int {
 	if !x.negative && y.negative {
 		return 1
@@ -7,56 +9,12 @@ func (x *Real) Compare(y *Real) int {
 		return -1
 	}
 
-	p := x.precision
-	if y.precision < p {
-		p = y.precision
+	if x.exponent > y.exponent {
+		return 1
+	} else if x.exponent < y.exponent {
+		return -1
 	}
 
-	a, b, _ := adjust(x, y)
-
-	for i := range a {
-		if a[i] > b[i] {
-			if x.negative {
-				return -1
-			}
-			return 1
-		} else if a[i] < b[i] {
-			if x.negative {
-				return 1
-			}
-			return -1
-		}
-	}
-
-	return 0
-}
-
-func compareSignificand(a, b []byte) int {
-	if len(a) != len(b) {
-		sc := len(a)
-		if len(b) < sc {
-			sc = len(b)
-		}
-		c := compareSignificand(a[:sc], b[:sc])
-		if c != 0 {
-			return c
-		}
-
-		// A normal significand never has trailing zeros, so we just
-		// check length here.
-		if len(a) > len(b) {
-			return 1
-		} else {
-			return -1
-		}
-	}
-	for i := range a {
-		if a[i] > b[i] {
-			return 1
-		} else if a[i] < b[i] {
-			return -1
-		}
-	}
-
-	return 0
+	// same exponents, just compare the significand
+	return bytes.Compare(x.significand, y.significand)
 }
--- compare_test.go
+++ compare_test.go
@@ -37,39 +37,3 @@ func TestCompare4(t *testing.T) {
 		t.Fatal("invalid compare")
 	}
 }
-
-func TestCompareSignificand1(t *testing.T) {
-	x := []byte{1, 2, 3, 4}
-	y := []byte{0, 0, 1, 1}
-
-	if compareSignificand(x, y) != 1 {
-		t.Fatal("invalid compare")
-	}
-}
-
-func TestCompareSignificand2(t *testing.T) {
-	x := []byte{1, 2, 3, 4}
-	y := []byte{3, 0, 1, 1}
-
-	if compareSignificand(x, y) != -1 {
-		t.Fatal("invalid compare")
-	}
-}
-
-func TestCompareSignificand3(t *testing.T) {
-	x := []byte{1, 2}
-	y := []byte{1, 2, 1, 1}
-
-	if compareSignificand(x, y) != -1 {
-		t.Fatal("invalid compare")
-	}
-}
-
-func TestCompareSignificand4(t *testing.T) {
-	x := []byte{1, 2, 1, 1}
-	y := []byte{1, 2}
-
-	if compareSignificand(x, y) != 1 {
-		t.Fatal("invalid compare")
-	}
-}
